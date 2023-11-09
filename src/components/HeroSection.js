45997d7b7d8dc8a9e1288869dc447d16f37784af
abbas <abbasaffandi2002@gmail.com>
2023-11-09 08:25:57 -0800

    ev-load

---
+++ src/components/HeroSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Navbar from "./navbar";
+import Us from "../images/US.svg";
+import Ca from "../images/CA.svg";
+import HeroImage from "../images/siemens-chargers-with-logo.svg";
+import Background from "../images/Vector 3.svg"
+import Arrow from "../images/Arrow - Right 2.svg"
+function HeroSection() {
+  return (
+    <div className="HeroSection relative">
+      <div className="container">
+        <Navbar />
+        <div className="container container-hero margin-left absolute">
+          <div className="max-w-[550px] mt-[-100px]">
+            <p className="font-bold mx-5 bottom-20 text-[#000000] font-[Montserrat,sans-serif] capitalize text-[34px]  text-left">
+              Accelerate<span className="text-[#4BAC88]"> EV charging</span>
+              <span className="bg-white px-6 py-1 block rounded-full mx-[-24px]">
+                infrastructure deployment{" "}
+              </span>
+              <div className="flex items-center">
+                <span className="capitalize font-extralight">
+                  and <span className="font-bold">permitting in </span>
+                </span>
+                <img src={Ca} alt="" className="w-[40px] mt-3" />
+                <img src={Us} alt="" className="w-[40px] mt-3" />
+              </div>
+            </p>
+            <p className="max-w-[420px] text-[12px] m-auto my-3">
+              For a fraction of time and cost, determine your site’s electrical
+              charging capacity with our NEC-Compliant cloud native solution
+            </p>
+            <div className='flex max-lg:flex-col'>
+            <div className="lg:mx-6 inline-flex gap-[6px] px-[30px] py-[10px] bg-[#000000] text-white border-1 border-[#00000000] rounded-[50px] hover:bg-[#4BAC88] transition-all cursor-pointer max-lg:w-[100%] max-lg:mb-4">
+            <button className="">See A Demo</button>
+            <img src={Arrow} alt=''/>
+            </div>
+             <div className="inline-flex gap-[6px] px-[30px] py-[10px] bg-[#4BAC88] text-white border-1 border-[#00000000] rounded-[50px] hover:bg-[#000000] transition-all cursor-pointer max-lg:w-[100%] max-lg:mb-4">
+            <button className="">Request A free Site Analysis</button>
+            <img src={Arrow} alt=''/>
+            </div>
+            </div>
+          </div>
+          <div className="h-full">
+            <img src={Background} alt='' className="absolute w-[200px] mx-[9%] mt-[-150px]"/>
+            <img src={HeroImage} alt="" className=" relative w-[400px] z-50" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "../images/Frame 82.svg";
+import IconlyLightArrow from "../images/Arrow - Right 2.svg";
+import Frame from "../images/Frame.svg"
+
+function navbar() {
+  return (
+    <div className="py-5 flex justify-around items-center z-100">
+      <div className="label flex justify-start items-center">
+        <img src={Image} alt="" className="iconly-light-arrow-right-2" />
+        <p className="EV-load">
+          <span className="text-wrapper">EV </span>
+          <span className="span">Load</span>
+        </p>
+      </div>
+        <img src={Frame} alt='' className="max-lg:flex justify-end lg:hidden cursor-pointer"/>
+      <div className="rounded-[100px] flex flex-row justify-between items-center max-lg:hidden">
+            <ul className="box bg-white m-0 px-10 py-4 gap-12 flex justify-center border-[0px] rounded-[100px] w-[400px]">
+                <li className="cursor-pointer hover-link">Home</li>
+                <li className="cursor-pointer hover-link">Product</li>
+                <li className="cursor-pointer hover-link">Partnering</li>
+                <li className="cursor-pointer hover-link">Contact</li>
+            </ul>
+      </div>
+      <div className="flex justify-end gap-2 items-center z-50 max-lg:hidden">
+        <button className="btn">
+          <div className="text-wrapper">GET A REPORT</div>
+          <img
+            src={IconlyLightArrow}
+            className="iconly-light-arrow-right-2"
+            alt=""
+          />
+        </button>
+        <button className="btn-two">
+          <div className="log-in">LOG IN</div>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
