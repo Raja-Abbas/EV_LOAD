34fdf55e5ff527f08be3adbe7f06f6965b5928fe
abbas <abbasaffandi2002@gmail.com>
2023-11-10 12:20:21 -0800

    ev-load

--- src/components/LineArrow.js
+++ src/components/LineArrow.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import LineArrowImage from "../images/LINE ARROW.svg"
+import React from "react";
+import LineArrowImage from "../images/LINE ARROW.svg";
 
 function LineArrow() {
   return (
     <div>
-        <img src={LineArrowImage} alt='' className='ml-60 mt-[-114px] lg:w-[52%] mx-auto'/>
+      <img
+        src={LineArrowImage}
+        alt=""
+        className="lg:ml-64 mt-[-114px] lg:w-[50%] mx-auto xl:ml-[360px] xl:w-[42%] min-[1350px]:w-[41%] min-[1350px]:ml-[380px] min-[1500px]:ml-[450px] min-[1500px]:w-[38%] min-[1600px]:ml-[745px] min-[1600px]:w-[26%]"
+      />
     </div>
-  )
+  );
 }
 
-export default LineArrow
+export default LineArrow;
