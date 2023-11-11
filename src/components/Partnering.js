dd2a32f839075001c51b075191b22655aa18110a
abbas <abbasaffandi2002@gmail.com>
2023-11-11 07:42:31 -0800

    ev-load

--- src/components/Partnering.js
+++ src/components/Partnering.js
@@ -1,18 +1,130 @@
-import React from 'react'
+import React from "react";
+import ImageOne from "../images/Image1.svg";
+import ImageTwo from "../images/Image2.svg";
+import ImageThree from "../images/Image3.svg";
+import ImageFour from "../images/Image4.svg";
+import ImageFive from "../images/Image5.svg";
+import ImageSix from "../images/Image6.svg";
+import ImageSeven from "../images/Image7.svg";
+import ImageEight from "../images/Image8.svg";
+import ArrowGreen from "../images/ArrowGreen.svg"
 
 function Partnering() {
   return (
-    <div className="bg-gray-100 mt-10 border border-t-[#4bac89ca] border-b-[#4bac89ca] max-lg:flex max-lg:flex-col max-lg:justify-center">
+    <div className="bg-gray-100 mt-10 border mb-10 pb-10 border-t-[#4bac89ca] border-b-[#4bac89ca] max-lg:flex max-lg:flex-col max-lg:justify-center">
       <div className="max-w-[1400px] lg:w-[900px] mx-auto">
         <p className="px-[30px] py-[10px] mt-[-25px] w-[300px] mx-auto bg-white border-[1px] border-[#4BAC88] text-black rounded-full text-[30px] leading-[30px] font-bold text-center">
           Partnering
         </p>
+      </div>
+      <div className="xl:max-w-[1460px] mx-auto flex justify-center sm:gap-4 lg:gap-x-4 xl:gap-x-8 flex-wrap gap-y-32 mt-16">
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageOne} alt="" className="" />
+          <p className="text-[#000000] text-[24px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">EV Charging </span>Infrastructure Developers
+          </p>
+          <p className="text-[16px] font-[400] leading-[30px] mt-2">
+            Leading the charge in creating a sustainable EV charging infrastructure worldwide.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
         </div>
-        <div className='bg-black mx-auto max-w-[1400px] grid lg:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1'>
-            
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageTwo} alt="" className="" />
+          <p className="text-[#000000] text-[26px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">Energy Storage</span> Developers
+          </p>
+           <p className="text-[16px] font-[400] leading-[30px] mt-2">
+            Innovative energy storage developers powering a sustainable future for electric vehicle charging.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
         </div>
         </div>
-  )
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageThree} alt="" className="" />
+          <p className="text-[#000000] text-[26px] capitalize font-bold mt-4">
+            Utilities
+          </p>
+           <p className="text-[16px] font-[400] leading-[30px] mt-2">
+            Your one-stop source for real-time EV charging data, rates, and availability to help optimize your electric vehicle charging.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageFour} alt="" className="" />
+          <p className="text-[#000000] text-[26px] capitalize font-bold mt-4 pe-12">
+            <span className="text-[#4BAC88]">Infrastructure </span>  Finance Partners
+          </p>
+           <p className="text-[16px] font-[400] leading-[30px] mt-2">
+            Connecting EV owners to state and federal funding agencies for efficient charging.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageFive} alt="" className="" />
+           <p className="text-[#000000] text-[24px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">Engineering </span>Firms
+          </p>
+          <p className="text-[16px] font-[400] leading-[30px] mt-2 me-4">
+            Top engineering firms specializing in electric vehicle infrastructure, innovation, and sustainable energy solutions. Leading the EV revolution.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageSix} alt="" className="" />
+          <p className="text-[#000000] text-[24px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">Permitting </span>Authorities
+          </p>
+          <p className="text-[16px] font-[400] leading-[30px] mt-2 me-4">
+            Connect with local permitting authorities for streamlined EV charging infrastructure approval and compliance guidance.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageSeven} alt="" className="" />
+          <p className="text-[#000000] text-[24px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">Business </span>Owners
+          </p>
+          <p className="text-[16px] font-[400] leading-[30px] mt-2 me-4">
+            Connects business owners to EV charging solutions, simplifying sustainable energy adoption for workplaces and fleets.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+        <div className="flex flex-col max-w-[300px] min-w-[250px]">
+          <img src={ImageEight} alt="" className="" />
+           <p className="text-[#000000] text-[24px] capitalize font-bold mt-4">
+            <span className="text-[#4BAC88]">Fleet </span>Managers
+          </p>
+          <p className="text-[16px] font-[400] leading-[30px] mt-2 me-4">
+           Expert fleet managers optimizing electric vehicle operations, enhancing efficiency, and promoting sustainable transportation on our website.
+        </p>
+        <div className="flex gap-4 mt-4 items-center hover:gap-2 transition-all">
+            <p className="text-[#4BAC88] text-[16px] font-[700] hover:underline cursor-pointer">Contact us</p>
+            <img src={ArrowGreen} alt=""/>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Partnering
+export default Partnering;
