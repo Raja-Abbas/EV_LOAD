86e6a49ad851df302c58b7f4b5a6e32df69d1a12
abbas <abbasaffandi2002@gmail.com>
2023-11-16 10:37:36 -0800

    first commit

--- src/components/HeroSectionComponent.js
+++ src/components/HeroSectionComponent.js
@@ -44,28 +44,30 @@ function HeroSectionComponent() {
             <img
               src={Image}
               alt=""
-              className="absolute max-lg:top-10 max-lg:left-0 max-lg:right-0 max-lg:mx-auto z-50 max-w-[360px] max-sm:px-4 lg:left-[600px] xl:top-16 lg:top-24 xl:left-[700px] min-[1350px]:left-[750px] min-[1500px]:left-[850px] min-[1600px]:left-[1100px]"
+              className="absolute image max-lg:top-10 max-lg:left-0 max-lg:right-0 max-lg:mx-auto z-50 max-w-[360px] max-sm:px-4 lg:left-[600px] xl:top-16 lg:top-24 xl:left-[700px] min-[1350px]:left-[750px] min-[1500px]:left-[850px] min-[1600px]:left-[900px] min-[1800px]:left-[980px]"
             />
-            <img src={Round} alt="" className="absolute max-lg:hidden z-50 max-w-[360px] max-sm:px-4 lg:right-[-36px] xl:-bottom-8 lg:-bottom-8 xl:right-[96px] min-[1350px]:right-[138px] min-[1500px]:right-[212px] min-[1600px]:right-[462px]"/>
+            <img src={Round} alt="" className="absolute round max-lg:hidden z-50 max-w-[360px] max-sm:px-4 lg:right-[-36px] xl:-bottom-8 min-[1050px]:right-[-14px] min-[1100px]:right-[8px] min-[1150px]:right-[34px] min-[1200px]:right-[60px]
+            min-[1250px]:right-[86px] min-[1300px]:right-[120px] 
+            lg:-bottom-8 xl:right-[96px] min-[1350px]:right-[140px] min-[1400px]:right-[172px] min-[1450px]:right-[200px] min-[1500px]:right-[206px] min-[1550px]:right-[238px] min-[1600px]:right-[270px] min-[1650px]:right-[300px] min-[1700px]:right-[313px] min-[1750px]:right-[344px] min-[1800px]:right-[364px] min-[1850px]:right-[396px] min-[1900px]:right-[426px]"/>
           </div>
-          <div className="green max-lg:w-full max-lg:h-[200px] max-lg:-top-32 lg:w-[38%] 2xl:w-[100%] lg:h-[351px] absolute right-0 z-[1] lg:left-[800px] xl:left-[900px] min-[1350px]:left-[950px] min-[1500px]:left-[1050px] min-[1600px]:left-[1300px]">
-            <div></div>
+          <div className="green max-lg:w-full max-lg:h-[200px] max-lg:-top-32 lg:w-[40%] min-[1600px]:w-[40%] min-[2000px]:w-[100%] lg:h-[351px] absolute right-0 z-[1] lg:left-[800px] xl:left-[900px] min-[1100px]:left-[840px] min-[1150px]:left-[860px] min-[1200px]:left-[880px] min-[1250px]:left-[910px] min-[1300px]:left-[930px] min-[1400px]:left-[970px] min-[1450px]:left-[990px] min-[1550px]:left-[1060px] min-[1900px]:w-[41.5%]
+          min-[1350px]:left-[950px] min-[1500px]:left-[1050px] min-[1600px]:left-[1080px] min-[1650px]:left-[1090px] min-[1700px]:left-[1130px] min-[1750px]:left-[1150px] min-[1800px]:left-[1180px] min-[1850px]:left-[1210px]">
           </div>
         </div>
       </div>
       <LineArrow />
       <div className="lg:mt-[-60px] max-lg:mt-[100px]">
         <div className="flex justify-center w-[100%] relative lg:flex-row max-lg:flex-col">
-          <div className="green green-two max-lg:w-[100%] max-lg:h-[200px] max-lg:-top-24 lg:w-[35%] h-[351px] absolute lg:left-[-120px] z-[1] right-0 rotate-180 xl:right-[150px] min-[1350px]:right-[250px] min-[1600px]:w-[35%] min-[1600px]:right-[350px] min-[2000px]:w-[100%] min-[2000px]:left-[-1280px]">
-            <div></div>
+          <div className="green green-two max-lg:w-[100%] max-lg:h-[200px] max-lg:-top-24 lg:w-[35%] h-[351px] absolute lg:left-[-120px] z-[1] right-0 rotate-180 xl:right-[150px] min-[1350px]:right-[250px] min-[1600px]:w-[35%] min-[1600px]:right-[350px] min-[2000px]:w-[100%] min-[2000px]:left-[-1280px]
+          min-[1100px]:left-[-180px] min-[1300px]:left-[-140px]">
           </div>
-          <div className="flex flex-col black black-other xl:max-w-[700px] lg:max-w-[680px] z-10 lg:ml-[250px] xl:left-[900px] min-[1350px]:left-[950px] min-[1500px]:left-[1050px] min-[1600px]:left-[1150px] max-lg:mt-52 max-lg:pt-32">
+          <div className="flex flex-col black black-other xl:max-w-[700px] lg:max-w-[680px] z-10 lg:ml-[250px] xl:left-[900px] min-[1350px]:left-[950px] min-[1500px]:left-[1050px] max-lg:mt-52 max-lg:pt-32">
             <img
               src={ImageSecond}
               alt=""
-              className="absolute z-50 max-w-[360px] lg:left-[100px] max-lg:top-[50px] max-lg:mx-auto lg:top-16 xl:left-[200px] min-[1350px]:left-[250px] min-[1500px]:left-[300px] min-[1600px]:left-[550px] max-lg:right-0 max-lg:left-0 max-sm:px-4"
+              className="absolute z-50 max-w-[360px] lg:left-[100px] max-lg:top-[50px] max-lg:mx-auto lg:top-16 xl:left-[200px] min-[1350px]:left-[250px] min-[1500px]:left-[300px] min-[1600px]:left-[300px] max-lg:right-0 max-lg:left-0 max-sm:px-4"
             />
-             <img src={Round} alt="" className="absolute max-lg:hidden z-50 max-w-[360px] max-sm:px-4 lg:left-[224px] xl:top-14 lg:top-14 xl:left-[328px] min-[1350px]:left-[348px] min-[1500px]:left-[418px] min-[1600px]:left-[712px]"/>
+             <img src={Round} alt="" className="absolute max-lg:hidden z-50 max-w-[360px] max-sm:px-4 lg:left-[224px] xl:top-14 lg:top-14 xl:left-[328px] min-[1350px]:left-[348px] min-[1500px]:left-[418px] min-[1600px]:left-[418px]"/>
             <p className="text-white text-[15px] font-[500] capitalize text-opacity-80">
               Benefits
             </p>
