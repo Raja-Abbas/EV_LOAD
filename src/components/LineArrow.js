fbe703fc4ea26d70ef3aaeca41675fbacf343139
abbas <abbasaffandi2002@gmail.com>
2023-11-16 11:50:20 -0800

    second commit

--- src/components/LineArrow.js
+++ src/components/LineArrow.js
@@ -7,7 +7,7 @@ function LineArrow() {
       <img
         src={LineArrowImage}
         alt=""
-        className="max-lg:hidden lg:ml-64 mt-[-114px] lg:w-[50%] mx-auto xl:ml-[360px] xl:w-[42%] min-[1350px]:w-[41%] min-[1350px]:ml-[380px] min-[1500px]:ml-[450px] min-[1500px]:w-[38%] min-[1600px]:ml-[450px] min-[1600px]:w-[38%] min-[1700px]:w-[39%] min-[1800px]:w-[39.5%]"
+        className="max-lg:hidden lg:ml-64 mt-[-114px] lg:w-[50%] mx-auto xl:ml-[360px] xl:w-[42%] min-[1350px]:w-[41%] min-[1350px]:ml-[380px] min-[1500px]:ml-[450px] min-[1500px]:w-[38%] min-[1600px]:ml-[450px] min-[1600px]:w-[38%] min-[1700px]:w-[39%] min-[1800px]:w-[39.5%] min-[1950px]:w-[40%]"
       />
     </div>
   );
